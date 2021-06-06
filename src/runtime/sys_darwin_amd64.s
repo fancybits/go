6b9ad0c024db55e63664b3080a0e6b2567d50bf4
Aman Karmani <aman@tmm1.net>
2021-06-05 23:04:21 -0700

    Revert "runtime: use clock_gettime instead of gettimeofday on darwin"
    Restore macOS 10.11 compat
    
    This reverts commit ae76f6e96216f352cc5021a4c8a7d879c4cb6873.

--- src/runtime/sys_darwin_amd64.s
+++ src/runtime/sys_darwin_amd64.s
@@ -10,8 +10,6 @@
 #include "go_tls.h"
 #include "textflag.h"
 
-#define CLOCK_REALTIME		0
-
 // Exit the entire program (like C exit)
 TEXT runtime·exit_trampoline(SB),NOSPLIT,$0
 	PUSHQ	BP
@@ -142,9 +140,9 @@ initialized:
 TEXT runtime·walltime_trampoline(SB),NOSPLIT,$0
 	PUSHQ	BP			// make a frame; keep stack aligned
 	MOVQ	SP, BP
-	MOVQ	DI, SI			// arg 2 timespec
-	MOVL	$CLOCK_REALTIME, DI	// arg 1 clock_id
-	CALL	libc_clock_gettime(SB)
+	// DI already has *timeval
+	XORL	SI, SI // no timezone needed
+	CALL	libc_gettimeofday(SB)
 	POPQ	BP
 	RET
 
