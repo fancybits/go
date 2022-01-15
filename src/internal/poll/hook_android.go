2c96b7a1929238e08f73c575de14e6e5fa8933c8
Aman Karmani <aman@tmm1.net>
2022-01-14 17:49:28 -0800

    HACK: ignore EPERM from close() on Android
    This works around an issue with NVIDIA's SHIELD Experience 9.0 release
    (based on Android 11), where EPERM is extraneously returned from close
    even though it succeeds.
    
    (cherry picked from commit d0434fbe3b30e5339febf8b14dfbe8301d2416b6)
    (cherry picked from commit fbaaa80a40b4669a483a7060e14a0150b1409f87)

--- src/internal/poll/hook_android.go
+++ src/internal/poll/hook_android.go
@@ -1,15 +1,22 @@
-// Copyright 2017 The Go Authors. All rights reserved.
+// Copyright 2021 The Go Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//go:build (unix && !android) || (js && wasm) || wasip1
+//go:build android
+// +build android
 
 package poll
 
 import "syscall"
 
 // CloseFunc is used to hook the close call.
-var CloseFunc func(int) error = syscall.Close
+func CloseFunc(fd int) error {
+	err := syscall.Close(fd)
+	if err == syscall.EPERM {
+		return nil
+	}
+	return err
+}
 
 // AcceptFunc is used to hook the accept call.
 var AcceptFunc func(int) (int, syscall.Sockaddr, error) = syscall.Accept
