9ef6be90f51aac1a458e5c2e44aca30f096cc2f2
Aman Karmani <aman@tmm1.net>
2022-01-20 09:35:18 -0800

    quiet handshake error logging

--- src/net/http/server.go
+++ src/net/http/server.go
@@ -1854,7 +1854,7 @@ func (c *conn) serve(ctx context.Context) {
 				re.Conn.Close()
 				return
 			}
-			c.server.logf("http: TLS handshake error from %s: %v", c.rwc.RemoteAddr(), err)
+			//c.server.logf("http: TLS handshake error from %s: %v", c.rwc.RemoteAddr(), err)
 			return
 		}
 		// Restore Conn-level deadlines.
