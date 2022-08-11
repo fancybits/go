9d44c4f76ea812b6f578b63185123c8b815c4eef
Aman Karmani <aman@tmm1.net>
2022-08-11 13:25:13 -0700

    runtime: fix build on aarch64-apple-ios-macabi
    (cherry picked from commit 864ec9b07d1020703076e667fec4f0f3c02988c9)

--- src/runtime/cgo/gcc_darwin_arm64.c
+++ src/runtime/cgo/gcc_darwin_arm64.c
@@ -15,7 +15,7 @@
 
 #include <TargetConditionals.h>
 
-#if TARGET_OS_IPHONE
+#if TARGET_OS_IPHONE && !TARGET_OS_MACCATALYST
 #include <CoreFoundation/CFBundle.h>
 #include <CoreFoundation/CFString.h>
 #endif
@@ -60,7 +60,7 @@ threadentry(void *v)
 	ts = *(ThreadStart*)v;
 	free(v);
 
-#if TARGET_OS_IPHONE
+#if TARGET_OS_IPHONE && !TARGET_OS_MACCATALYST
 	darwin_arm_init_thread_exception_port();
 #endif
 
@@ -68,7 +68,7 @@ threadentry(void *v)
 	return nil;
 }
 
-#if TARGET_OS_IPHONE
+#if TARGET_OS_IPHONE && !TARGET_OS_MACCATALYST
 
 // init_working_dir sets the current working directory to the app root.
 // By default ios/arm64 processes start in "/".
@@ -122,7 +122,7 @@ init_working_dir()
 	}
 }
 
-#endif // TARGET_OS_IPHONE
+#endif // TARGET_OS_IPHONE && !TARGET_OS_MACCATALYST
 
 void
 x_cgo_init(G *g, void (*setg)(void*))
@@ -134,7 +134,7 @@ x_cgo_init(G *g, void (*setg)(void*))
 	size = pthread_get_stacksize_np(pthread_self());
 	g->stacklo = (uintptr)&size - size + 4096;
 
-#if TARGET_OS_IPHONE
+#if TARGET_OS_IPHONE && !TARGET_OS_MACCATALYST
 	darwin_arm_init_mach_exception_handler();
 	darwin_arm_init_thread_exception_port();
 	init_working_dir();
